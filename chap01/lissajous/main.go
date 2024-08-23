0da8c89b23938c6c2fff45fd4842f6a5fc4dec24
Đặng Quang Hưng <hungdq58@gmail.com>
2024-08-23 11:53:37 +0700

    Refactor lissajous

--- chap01/lissajous/main.go
+++ chap01/lissajous/main.go
@@ -2,20 +2,9 @@ package main
 
 import (
 	"fmt"
-	"image"
-	"image/color"
-	"image/gif"
-	"io"
-	"math"
-	"math/rand"
 	"os"
-)
 
-var palette = []color.Color{color.White, color.Black}
-
-const (
-	whiteIndex = 0
-	blackIndex = 1
+	lissajous "github.com/hungqd/go-learning/chap01/lissajous/lib"
 )
 
 func main() {
@@ -25,37 +14,10 @@ func main() {
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "Error: %v", err)
 		} else {
-			lissajous(file)
+			lissajous.Lissajous(file)
 			file.Close()
 		}
 	} else {
-		lissajous(os.Stdout)
+		lissajous.Lissajous(os.Stdout)
 	}
-}
-
-func lissajous(out io.Writer) {
-	const (
-		cycles  = 5
-		res     = 0.001
-		size    = 100
-		nframes = 64
-		delay   = 8
-	)
-
-	freq := rand.Float64() * 3.0
-	anim := gif.GIF{LoopCount: nframes}
-	phase := 0.0
-	for i := 0; i < nframes; i++ {
-		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
-		img := image.NewPaletted(rect, palette)
-		for t := 0.0; t < cycles*2*math.Pi; t += res {
-			x := math.Sin(t)
-			y := math.Sin(t*freq + phase)
-			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5), blackIndex)
-		}
-		phase += 0.1
-		anim.Delay = append(anim.Delay, delay)
-		anim.Image = append(anim.Image, img)
-	}
-	gif.EncodeAll(out, &anim)
 }
