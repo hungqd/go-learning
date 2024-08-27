3741794585e5cb0c1b8cd40a626a8a97e96cf407
Đặng Quang Hưng <hungdq58@gmail.com>
2024-08-27 16:16:47 +0700

    Clock 2 - Add port & timezone params

--- chap08/clock2/clock.go
+++ chap08/clock2/clock.go
@@ -1,32 +1,51 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"io"
 	"log"
 	"net"
+	"os"
 	"time"
 )
 
+var port = flag.Int("port", 8000, "port number")
+
 func main() {
-	listener, err := net.Listen("tcp", "localhost:8000")
+	tz, ok := os.LookupEnv("TZ")
+	if !ok {
+		tz = "Local"
+	}
+	location, err := time.LoadLocation(tz)
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	flag.Parse()
+	listener, err := net.Listen("tcp", fmt.Sprintf("localhost:%d", *port))
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Server is listening on port %d...\n", *port)
+
 	for {
 		conn, err := listener.Accept()
 		if err != nil {
 			log.Fatal(err)
 			continue
 		}
-		go handleConnection(conn)
+		go handleConnection(conn, location)
 	}
 }
 
-func handleConnection(c net.Conn) {
+func handleConnection(c net.Conn, loc *time.Location) {
+	fmt.Printf("Handle connection... Remote addr: %v\n", c.RemoteAddr())
 	defer c.Close()
 	for {
-		_, err := io.WriteString(c, time.Now().Format("15:04:05\n"))
+		_, err := io.WriteString(c, time.Now().In(loc).Format("15:04:05\n"))
 		if err != nil {
+			fmt.Printf("Client disconnected. %v\n", c.RemoteAddr())
 			return
 		}
 		time.Sleep(1 * time.Second)
