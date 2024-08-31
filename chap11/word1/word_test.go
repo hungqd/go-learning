f08fc969a1ebf934a16d57c5bfaaeca7567e0d9b
Đặng Quang Hưng <hungdq58@gmail.com>
2024-08-31 21:24:47 +0700

    Benchmarking

--- chap11/word1/word_test.go
+++ chap11/word1/word_test.go
@@ -32,3 +32,9 @@ func TestIsPalindrome(t *testing.T) {
 		}
 	}
 }
+
+func BenchmarkIsPalindrome(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		word.IsPalindrome("A man, a plan, a canal: Panama")
+	}
+}
